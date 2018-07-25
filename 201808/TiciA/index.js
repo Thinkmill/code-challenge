f63f93fe8543e8d11a3e927a28c7dffa7187124c
ticidesign <ticianadeandrade@Ticianas-MacBook-Pro.local>
2018-07-25 16:32:41 +1000

    Removing comments

--- 201808/TiciA/index.js
+++ 201808/TiciA/index.js
@@ -14,7 +14,6 @@ class BOT {
 	}
 
 	OnTurn({ history, myCards, myCoins, otherPlayers, discardedCards }) {
-		// let action = ACTIONS()[ Math.floor( Math.random() * ACTIONS().length ) ];
 		let action = ['taking-1', 'foreign-aid'][ Math.floor( Math.random() * 2) ];
 		let against = otherPlayers[ Math.floor( Math.random() * otherPlayers.length ) ].name;
 
@@ -24,10 +23,6 @@ class BOT {
 
 		this.ROUND ++;
 		
-		//In the early game, you want to be the Duke so you can gain the coin advantage. 
-		//The Assassin can be nice to knock opponents down in influence early and 
-		//The Contessa can help to block early assassination attempts as well. 
-		//If you are none of these, you may want to take the Ambassador action and exchange your cards with the Court Deck.
 		if (this.ROUND <= 2) {
 
 			if(myCards.includes('duke')) {
@@ -67,9 +62,6 @@ class BOT {
 
 	OnCounterAction({ history, myCards, myCoins, otherPlayers, discardedCards, action, byWhom }) {
 		if( action === 'assassination' ) {
-			//If you have 2 influence, NEVER:
-			//Falsely claim to be the Contessa when you are being assassinated.
-			//Challenge an Assassin who is assassinating you.
 			if (myCards.length === 2 && myCards.indexOf('contessa') === -1) {
 				return false;
 			} else if (myCards.includes('contessa')) {
@@ -97,22 +89,6 @@ class BOT {
 	}
 
 	OnSwappingCards({ history, myCards, myCoins, otherPlayers, discardedCards, newCards }) {
-		// let optionCards = myCards.concat(newCards);
-		// let newHand = [];
-		// // let randowCard = newCards[ Math.floor( Math.random() * (newHand.length) )]
-
-		// //remove duplicated 
-		// optionCards.forEach( card => {
-		// 	if (newHand.indexOf(card) < 0) {
-		// 		return newHand.push(card);
-		// 	}
-		// });
-		// console.log('------------------------------------');
-		// console.log(myCards, newCards, newHand);
-		// console.log('------------------------------------');
-		// if (myCards.includes('captain')) {
-		// 	 return myCards;
-		// }
 
 		if(myCards.length === 2) {
 			return newCards;
