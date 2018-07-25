38cc01ce902924a0f303e8c479be925887f67188
Joss Mackison <joss.mackison@gmail.com>
2018-07-25 22:22:32 +1000

    cleanup before improvement

--- 201808/JossM/index.js
+++ 201808/JossM/index.js
@@ -2,250 +2,119 @@
 
 const { ALLBOTS, CARDS, DECK, ACTIONS } = require('../constants.js');
 
-function selectRandomFrom(list, padWith) {
-	const idx = Math.floor(Math.random() * list.length);
+const CARD_PREFERENCE = [
+	'duke',
+	'captain',
+	'ambassador',
+	'contessa',
+	'assassin',
+];
 
-	// pad out the array
-	let arr = list;
-	if (padWith) {
-		const padding = new Array(padWith.count).fill(padWith.value);
-		arr = [...padding, ...list];
-	}
+// ==============================
+// Utils
+// ==============================
 
-	return arr[idx];
+function unique(value, index, self) {
+	return self.indexOf(value) === index;
 }
-function hasCard(card, myCards) {
-	return myCards.indexOf(card) > -1;
+function clone(arr) {
+	return arr.slice(0);
 }
-function getCardCount(card, fromCards) {
-	return fromCards.filter((c) => c === card).length;
+
+// ==============================
+// Helpers
+// ==============================
+
+function count(card, cards) {
+	return cards.filter(c => c === card).length;
 }
-function isCardAvailable(card, fromCards) {
-	const count = getCardCount(card, fromCards);
-	return count < 3;
+function loseCard(cards) {
+	const preference = clone(CARD_PREFERENCE).reverse();
+	return preference.filter(c => cards.includes(c))[0];
 }
+function swapCard(current, possible) {
+	const combined = [...current, ...possible];
+	const uniqueCards = combined.filter(unique);
+	const preference = clone(CARD_PREFERENCE);
+	return preference.filter(c => uniqueCards.includes(c)).slice(0, 2);
+}
+function getCardFor(action) {
+	const actions = {
+		'taking-3': 'duke',
+		assassination: 'assassin',
+		stealing: 'captain',
+		swapping: 'ambassador',
+	};
+
+	return actions[action];
+}
+function getBlockersFor(action) {
+	const actions = {
+		assassination: ['contessa'],
+		stealing: ['captain', 'ambassador'],
+		'foreign-aid': ['duke'],
+	};
+
+	return actions[action];
+}
+function getTarget(players) {
+	const byCoin = (a, b) => b.coins - a.coins;
+	const withTwo = players.filter(p => p.cards === 2);
+	const withOne = players.filter(p => p.cards === 1);
+	const arr = withTwo.length ? withTwo.sort(byCoin) : withOne.sort(byCoin);
+
+	return arr[0];
+}
+
+// ==============================
+// Game
+// ==============================
 
 class BOT {
-	cacheGameState({ history, myCards, myCoins, otherPlayers, discardedCards }) {
-		// self
-		this.hasAmbassador = hasCard('ambassador', myCards);
-		this.hasAssassin = hasCard('assassin', myCards);
-		this.hasCaptain = hasCard('captain', myCards);
-		this.hasContessa = hasCard('contessa', myCards);
-		this.hasDuke = hasCard('duke', myCards);
-
-		// other
-		this.playersByCoins = otherPlayers
-			.sort((a, b) => a.coins - b.coins)
-			.reverse();
-
-		this.playersByCards = otherPlayers.sort((a, b) => a.cards - b.cards);
-	}
-
-	// ==============================
-	// Game
-	// ==============================
-
-	OnTurn(props) {
-		this.cacheGameState(props); // cache first
-
-		// ==============================
-		// setup turn
-		// ==============================
-
-		const { history, myCards, myCoins, otherPlayers, discardedCards } = props;
-
-		// avoid attempting a coup/assassination without enough coins
-		let safeActs = ACTIONS();
-		if (myCoins < 7) safeActs = safeActs.filter((a) => a !== 'couping');
-		if (myCoins < 3) safeActs = safeActs.filter((a) => a !== 'assassination');
-
-		let action = selectRandomFrom(safeActs, { value: 'taking-1', count: 3 }); // pad with income to limit challenges
-		let against = selectRandomFrom(otherPlayers).name;
-
-		// ==============================
-		// definitive actions
-		// ==============================
-
-		// coup as soon as possible when one other player
-		if (otherPlayers.length === 1 && myCoins >= 7) {
-			action = 'couping';
-			against = otherPlayers[0].name;
-
-			return { action, against };
-		}
+	OnTurn({ history, myCards, myCoins, otherPlayers, discardedCards }) {
+		// attempt foreign-aid sometimes
+		let action = Math.random() > 0.5 ? 'taking-1' : 'foreign-aid';
+		let against = getTarget(otherPlayers).name;
 
 		// ==============================
 		// random actions
 		// ==============================
 
-		// steal coins, favour those who have more
-		if (this.hasCaptain) {
-			action = 'stealing';
-			against = this.playersByCoins[0].name;
-		}
-
-		// tax when possible
-		if (this.hasDuke) {
-			action = 'taking-3';
-		}
-
-		// assassinate when possible, favour those with fewer cards
-		if (this.hasAssassin && myCoins >= 3) {
-			action = 'assassination';
-			against = this.playersByCards[0].name;
-		}
-
-		// must coup after 10 coins, favour those with fewer cards
-		if (myCoins >= 10) {
+		if (myCoins >= 7) {
 			action = 'couping';
-			against = this.playersByCards[0].name;
+		} else if (myCards.includes(getCardFor('assassination')) && myCoins >= 3) {
+			action = 'assassination';
+		} else if (myCards.includes(getCardFor('swapping'))) {
+			action = 'swapping';
+		} else if (myCards.includes(getCardFor('stealing'))) {
+			action = 'stealing';
+		} else if (myCards.includes(getCardFor('taking-3'))) {
+			action = 'taking-3';
 		}
 
 		return { action, against };
 	}
 
-	OnChallengeActionRound({
-		action,
-		byWhom,
-		discardedCards,
-		history,
-		myCards,
-		myCoins,
-		otherPlayers,
-		toWhom,
-	}) {
-		const knownCards = [...myCards, ...discardedCards];
-		const assassinAvailable = isCardAvailable('assassin', knownCards);
-		const captainAvailable = isCardAvailable('captain', knownCards);
-		const ambassadorAvailable = isCardAvailable('ambassador', knownCards);
-
-		if (action === 'assassination' && !assassinAvailable) {
-			return true;
-		}
-		if (action === 'stealing' && !captainAvailable) {
-			return true;
-		}
-		if (action === 'swapping' && !ambassadorAvailable) {
-			return true;
-		}
-
-		return selectRandomFrom([true], { value: false, count: 3 });
+	OnChallengeActionRound({ action, discardedCards, myCards }) {
+		return count(getCardFor(action), [...myCards, ...discardedCards]) === 3;
 	}
 
-	OnCounterAction({
-		action,
-		byWhom,
-		discardedCards,
-		history,
-		myCards,
-		myCoins,
-		otherPlayers,
-	}) {
-		if (action === 'assassination') {
-			return this.hasContessa
-				? 'contessa'
-				: selectRandomFrom(['contessa'], { value: false, count: 3 });
-		}
-		if (action === 'foreign-aid') {
-			return this.hasDuke
-				? 'duke'
-				: selectRandomFrom(['duke'], { value: false, count: 5 });
-		}
-
-		if (action === 'stealing') {
-			if (this.hasAmbassador) {
-				return 'ambassador';
-			}
-			if (this.hasCaptain) {
-				return 'captain';
-			}
-
-			return selectRandomFrom(['ambassador', 'captain'], {
-				value: false,
-				count: 3,
-			});
-		}
+	OnCounterAction({ action, myCards }) {
+		const hasBlock = getBlockersFor(action).find(c => myCards.includes(c));
+		return hasBlock || false;
 	}
 
-	OnCounterActionRound({
-		action,
-		byWhom,
-		card,
-		discardedCards,
-		history,
-		myCards,
-		myCoins,
-		otherPlayers,
-		toWhom,
-	}) {
-		const knownCards = [...myCards, ...discardedCards];
-		const ambassadorAvailable = isCardAvailable('ambassador', knownCards);
-		const captainAvailable = isCardAvailable('captain', knownCards);
-		const contessaAvailable = isCardAvailable('contessa', knownCards);
-		const dukeAvailable = isCardAvailable('duke', knownCards);
-
-		if (action === 'foreign-aid' && !dukeAvailable) {
-			return true;
-		}
-		if (action === 'assassination' && !contessaAvailable) {
-			return true;
-		}
-		if (action === 'stealing' && !captainAvailable && !ambassadorAvailable) {
-			return true;
-		}
-
-		return selectRandomFrom([true], { value: false, count: 3 });
+	OnCounterActionRound({ card, discardedCards, myCards }) {
+		return count(card, [...myCards, ...discardedCards]) === 3;
 	}
 
-	OnSwappingCards({
-		discardedCards,
-		history,
-		myCards,
-		myCoins,
-		newCards,
-		otherPlayers,
-	}) {
-		const canHaveAssassin = hasCard('assassin', newCards);
-		const canHaveContessa = hasCard('contessa', newCards);
-
-		// try to gain assassin when possible
-		if (!this.hasAssassin && canHaveAssassin) {
-			let otherCard = myCards.filter((c) => c !== 'contessa')[0]; // avoid discarding contessa
-			otherCard = otherCard || myCards[0];
-			return [otherCard, 'assassin'];
-		}
-
-		// try to gain contessa when possible
-		if (!this.hasContessa && canHaveContessa) {
-			let otherCard = myCards.filter((c) => c !== 'assassin')[0]; // avoid discarding assassin
-			otherCard = otherCard || myCards[0];
-			return [otherCard, 'contessa'];
-		}
-
-		// let the universe decide whether to swap or hold...
-		return Math.random() > 0.5 ? newCards : myCards;
+	OnSwappingCards({ myCards, newCards }) {
+		return swapCard(myCards, newCards);
 	}
 
 	OnCardLoss({ history, myCards, myCoins, otherPlayers, discardedCards }) {
-		if (myCards.length === 1) {
-			return myCards[0];
-		}
-
-		// avoid discarding the contessa
-		if (this.hasContessa) {
-			let withoutContessa = myCards.filter((c) => c !== 'contessa');
-			return withoutContessa.length ? withoutContessa[0] : myCards[0];
-		}
-
-		// avoid discarding the assassin
-		if (this.hasAssassin) {
-			let withoutAssassin = myCards.filter((c) => c !== 'assassin');
-			return withoutAssassin.length ? withoutAssassin[0] : myCards[0];
-		}
-
-		// throwout whatever
-		return myCards[0];
+		if (myCards.length === 1) return myCards[0];
+		return loseCard(myCards);
 	}
 }
 
