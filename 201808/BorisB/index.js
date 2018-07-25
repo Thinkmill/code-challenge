fc688c34cb7422469e3aa4bdc794331ea018539c
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-07-25 16:40:33 +1000

    added missing people
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

---
+++ 201808/BorisB/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const {
+	ALLBOTS,
+	CARDS,
+	DECK,
+	ACTIONS,
+} = require('../constants.js');
+
+
+class BOT {
+	OnTurn({ history, myCards, myCoins, otherPlayers, discardedCards }) {
+		let action = ACTIONS()[ Math.floor( Math.random() * ACTIONS().length ) ];
+		const against = otherPlayers[ Math.floor( Math.random() * otherPlayers.length ) ].name;
+
+		if( myCoins > 10 ) {
+			action = 'couping';
+		}
+
+		return {
+			action,
+			against,
+		};
+	}
+
+	OnChallengeActionRound({ history, myCards, myCoins, otherPlayers, discardedCards, action, byWhom, toWhom }) {
+		return [ true, false ][ Math.floor( Math.random() * 2 ) ];
+	}
+
+	OnCounterAction({ history, myCards, myCoins, otherPlayers, discardedCards, action, byWhom }) {
+		if( action === 'assassination' ) {
+			return [ false, 'contessa' ][ Math.floor( Math.random() * 2 ) ];
+		}
+		else if( action === 'stealing' ) {
+			return [ false, 'ambassador', 'captain' ][ Math.floor( Math.random() * 3 ) ];
+		}
+	}
+
+	OnCounterActionRound({ history, myCards, myCoins, otherPlayers, discardedCards, action, byWhom, toWhom, card }) {
+		return [ true, false ][ Math.floor( Math.random() * 2 ) ];
+	}
+
+	OnSwappingCards({ history, myCards, myCoins, otherPlayers, discardedCards, newCards }) {
+		return newCards;
+	};
+
+	OnCardLoss({ history, myCards, myCoins, otherPlayers, discardedCards }) {
+		return myCards[ 0 ];
+	};
+}
+
+
+module.exports = exports = BOT;
